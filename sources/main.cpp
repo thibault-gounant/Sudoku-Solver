4af2ab82d70ab0d834c1d42f2efb5c5c4bf14d18
Thibault GOUNANT <thibault.gounant@etu.univ-nantes.fr>
2024-02-05 00:00:00 +0000

    Add SFML

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,25 @@
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
-using namespace std;
+
+#define WIDTH 800
+#define HEIGHT 600
+
 
 int main(int argc, char* argv[]) {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Sudoku Solver");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color(0, 0, 0));
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
